dcdf07e1b8ddc769fb570d2298f0780f6ab27fc3
vinicius pereira <viniciuslink877@gmail.com>
2022-03-09 20:39:59 -0300

    atividade0903

--- atividade0903v2.js
+++ atividade0903v2.js
@@ -1,8 +1,18 @@
 //programa que faz o teste lógico e o calculo de alunos
 
-for(var aluno = 0; aluno <= 14; aluno++){
+/*for(var aluno = 0; aluno <= 14; aluno++){
    if(aluno % 2==0){
        console.log("aluno " +aluno+ ": par");
    } else
        console.log("aluno " +aluno+ ": impar")
 }
+*/
+
+const aluno =14;
+for(var index=0; index <= 14; index++){
+    if(index % 2==0)
+        console.log("aluno nº: "+index+ "= par");
+    
+    else
+    console.log("aluno nº: "+index+ "= ímpar");
+} 
